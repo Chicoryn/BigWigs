cffd0e701d1df912483fd37a906bab34d5d26318
Karl Sundequist Blomdahl <karl.sundequist.blomdahl@gmail.com>
2020-01-03 02:02:37 +0100

    Count the number of Firesworn that has been killed on Garr

--- MoltenCore/Garr.lua
+++ MoltenCore/Garr.lua
@@ -3,11 +3,29 @@
 -- Module declaration
 --
 
-local mod = BigWigs:NewBoss("Garr", 409)
+local mod, CL = BigWigs:NewBoss("Garr", 409)
 if not mod then return end
 mod:RegisterEnableMob(12057)
 mod:SetAllowWin(true)
 mod.engageId = 666
+
+--------------------------------------------------------------------------------
+-- Locals
+--
+
+local fireswornDead = 0
+
+--------------------------------------------------------------------------------
+-- Localization
+--
+
+local L = mod:NewLocale("enUS", true)
+if L then
+	L.firesworn = "Firesworn"
+	L.firesworn_desc = "Warn for when a Firesworn is killed."
+	L.firesworn_icon = "inv_stone_10"
+end
+L = mod:GetLocale()
 
 --------------------------------------------------------------------------------
 -- Initialization
@@ -15,19 +33,31 @@ mod.engageId = 666
 
 function mod:GetOptions()
 	return {
+		"firesworn",
 		19492, -- Antimagic Pulse
 	}
+end
+
+function mod:OnEngage()
+	fireswornDead = 0
 end
 
 function mod:OnBossEnable()
 	self:Log("SPELL_CAST_SUCCESS", "Pulse", self:SpellName(19492))
 
 	self:Death("Win", 12057)
+	self:Death("FireswornDeaths", 12099)
 end
 
 --------------------------------------------------------------------------------
 -- Event Handlers
 --
+
+function mod:FireswornDeaths(args)
+	fireswornDead = fireswornDead + 1
+
+	self:Message("firesworn", "green", nil, CL.add_killed:format(fireswornDead, 8), "inv_stone_10")
+end
 
 function mod:Pulse(args)
 	self:Bar(19492, 18)
